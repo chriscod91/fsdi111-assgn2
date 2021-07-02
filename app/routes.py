6017fea629ffbef7e2c7de9c4b628b28ee8fdb0d
chriscod91 <gchriscod91@gmail.com>
2021-07-01 17:12:21 -0700

    added new pic

--- app/routes.py
+++ app/routes.py
@@ -1,70 +1,20 @@
 
 from flask import Flask, request, render_template
-from app.database import (
-    scan, insert, 
-    deactivate_user,
-    select_user,
-)
-
-
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///user.db"
+db = SQLAlchemy(app)
+from app.database import User
+
 
 @app.route("/")
 def get_index():
     return render_template("index.html")
     
-@app.route("/users")
-def GET_all_users():
-    out = {
-        "ok": True,
-        "Message": "Success"
-    } 
-    out["body"] = scan()
-    #retreive list of users
-    return out 
-
-@app.route("/users", methods=["POST"])
-def create_user():
-    out = {
-    "ok": True,
-    "Message": "Success"
-    } 
-    user_data = request.json
-    out["last_row_id"] = insert(
-        user_data.get("first_name"),
-        user_data.get("last_name"),
-        user_data.get("hobbies"),
-        user_data.get("active"),
-    )
-    return out, 201
-
-@app.route("/users/<int:uid>", methods=["DELETE"])
-def delete_user(uid):
-    out = {
-    "ok": True,
-    "Message": "Success"
-    } 
-    deactivate_user(uid)
-    return out, 204
-
-        
-@app.route('/agent')
-def agent():
-    user_agent = request.headers.get("csrftoken")
-    return "<p>Your user agent is: %s</p>" % user_agent
-
-@app.route("/users/<int:uid>", methods=["GET"])
+@app.route("/users/<int:uid>")
 def get_user(uid):
-   # out = {
-    #    "ok": True,
-     #   "message": "Success"
-    #}
-
-    user_list = select_user(uid)
-    if user_list:
-        return render_template("user_detail.html", user=user_list[0])
-    return render_template("404.html"), 404
+    user = User.query.filter_by(id=uid).first()
+    return render_template("user_detail.html", user=user)
 
 @app.errorhandler(404)
 def page_not_found(e):
